897f73bbc981d4a7a874312f88642abc674421b4
Ramindu <ramindu@ramindu-PC.(none)>
2012-08-08 12:30:24 +0530

    Added class initialization and header auth creation methods

--- zipmark.rb
+++ zipmark.rb
@@ -1,2 +1,23 @@
 require "rubygems"
 require "httparty"
+
+# Class to access Zipmark REST API
+class ZipMark
+  include HTTParty
+  base_url = "https://sandbox.zipmark.com"
+  
+  def initialize(app_id, app_secret)
+    @app_id = app_id
+    @app_secret = app_secret
+  end
+  
+  # Build header authorization block to send to given URI
+  def build_header_auth(uri)
+    @header = "Digest username:\"#{@app_id}\", 
+                          realm:\"Zipmark\", 
+                          uri:\"#{uri}\",
+                          nonce:\"#{@app_secret}\""
+  end
+end
+
+
