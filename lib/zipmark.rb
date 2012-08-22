ece6de0b4e634818b6d563c97c9a69adf58b4acb
Ramindu Deshapriya <rasade88@gmail.com>
2012-08-22 10:53:17 +0530

    Added v2 support for bill callbacks

--- lib/zipmark.rb
+++ lib/zipmark.rb
@@ -25,7 +25,7 @@ class ZipMark
   end
 
   # Build header authorization block to send to given URI
-  def build_header_auth(uri, version)
+  def build_header_auth(uri, version, httpmethod)
     response = get_auth_response(uri)
     @cnonce = Digest::MD5.hexdigest("%x" % (Time.now.to_i + rand(65535)))
     @@nonce_count += 1
@@ -94,7 +94,7 @@ class ZipMark
 
   # Method to get approval rules
   def get_approval_rules()
-    build_header_auth('/approval_rules', API_VERSION_1)
+    build_header_auth('/approval_rules', API_VERSION_1, 'GET')
     build_request()
     request = Net::HTTP::Get.new(@uri.request_uri)
     #puts "Request headers\n"
@@ -112,7 +112,7 @@ class ZipMark
 
   # Method to get vendor relationships
   def get_vendor_relationships()
-    build_header_auth('/vendor_relationships', API_VERSION_1)
+    build_header_auth('/vendor_relationships', API_VERSION_1, 'GET')
     build_request()
     request = Net::HTTP::Get.new(@uri.request_uri)
     @header.each do |name, value|
@@ -127,7 +127,7 @@ class ZipMark
   # Params should be formatted as follows:
   # id, amount_cents, bill_template_id, memo, content, recurring, customer_id, date
   def create_bill(params)
-    build_header_auth('/bills', API_VERSION_1)
+    build_header_auth('/bills', API_VERSION_1, 'POST')
     build_request()
     p_hash = {}
     p_hash["bill"] = {"identifier"        => params['id'],
@@ -153,8 +153,8 @@ class ZipMark
 
     # Bill callback request
     def bill_callback(params, api_version)
-      if version == API_VERSION_1
-        build_header_auth('/callback', 'POST')
+      if api_version == API_VERSION_1
+        build_header_auth('/callback', API_VERSION_1, 'POST')
         build_request()
         p_hash = {}
         p_hash["callback"] = {
@@ -170,7 +170,7 @@ class ZipMark
           "payee"           => params['payee'],
           "bill_encoded_id" => params['bill_encoded_id']
         }
-        req_body.p_hash.to_json.to_s
+        req_body = p_hash.to_json.to_s
         request = Net::HTTP::Post.new(@uri.request_uri)
         @header.each do |name,value|
           request[name] = value
@@ -178,24 +178,16 @@ class ZipMark
         request.set_body_internal(req_body)
         response = @http.request(request)
         return response
-      else if version == API_VERSION_2
-        build_header_auth('/callbacks', 'POST')
+      else if api_version == API_VERSION_2
+        build_header_auth('/callbacks', API_VERSION_2, 'POST')
         build_request()
         p_hash = {}
         p_hash["callback"] = {
-          "amount_cents"    => params['amount_cents'],
-          "bill_id"         => params['bill_id'],
-          "created_at"      => params['created_at'],
-          "date"            => params['date'],
-          "id"              => params['id'],
-          "memo"            => params['memo'],
-          "status"          => params['status'],
-          "bill_identifier" => params['bill_identifier'],
-          "customer_id"     => params['customer_id'],
-          "payee"           => params['payee'],
-          "bill_encoded_id" => params['bill_encoded_id']
+          "api_version"    => "v2",
+          "url"            => "https://curdbee.com/callbacks",
+          "event"          => "bill.update"
         }
-        req_body.p_hash.to_json.to_s
+        req_body = p_hash.to_json.to_s
         request = Net::HTTP::Post.new(@uri.request_uri)
         @header.each do |name,value|
           request[name] = value
